30dd143b64438780d416547a9b40deb83997407e
苏志权 <939958149@qq.com>
2022-03-29 12:17:49 +0800

    初始化项目

---
+++ test/custom-theme.js
@@ -0,0 +1,43 @@
+// custom-theme.js
+export default {
+    colors: {
+        background: "#222",
+        text: "#ddd",
+        primary: "#a66"
+    },
+    styles: {
+        CodeSurfer: {
+            pre: {
+                color: "text",
+                backgroundColor: "background"
+            },
+            code: {
+                color: "text",
+                backgroundColor: "background"
+            },
+            tokens: {
+                "comment cdata doctype": {
+                    fontStyle: "italic"
+                },
+                "builtin changed keyword punctuation operator tag deleted string attr-value char number inserted": {
+                    color: "primary"
+                },
+                "line-number": {
+                    opacity: 0.8
+                }
+            },
+            title: {
+                backgroundColor: "background",
+                color: "text"
+            },
+            subtitle: {
+                color: "#d6deeb",
+                backgroundColor: "rgba(10,10,10,0.9)"
+            },
+            unfocused: {
+                // only the opacity of unfocused code can be changed
+                opacity: 0.1
+            }
+        }
+    }
+};
